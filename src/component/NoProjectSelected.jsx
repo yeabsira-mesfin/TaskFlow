4f0dc2849988ff89f5342981a0b19d42efb0182d
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-20 15:15:17 -0400

    final

--- src/component/NoProjectSelected.jsx
+++ src/component/NoProjectSelected.jsx
@@ -5,7 +5,7 @@ const NoProjectSelected = ({onStartedProject}) => {
   return (
     <div className='mt-24 text-center w-2/3'>
         <img src={noProjectImg} alt="An empty task list" className='w-16 h-16 object-contain mx-auto'/>
-        <h2 className='text-xl font-bold text-white my-4'>No Porject Selected</h2>  
+        <h2 className='text-xl font-bold text-white my-4'>No Project Selected</h2>  
         <p className='mb-4 text-white'>Select a project or get started with a new one</p> 
         <p>
            <Button onClick={onStartedProject}>Create new project</Button>
