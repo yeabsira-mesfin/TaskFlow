d1c046f7185387fdcd8ed52155f3b0a954e9145d
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-20 05:01:31 -0400

    Sidebar Initial

---
+++ src/component/ProjectSidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProjectSidebar = () => {
+  return (
+    <aside>
+        <h2>Your projects</h2>
+        <div>
+            <button>
+                + Add Project
+            </button>
+        </div>
+        <ul></ul>
+    </aside>
+  )
+}
+
+export default ProjectSidebar
