be7fcd9e1808f572a77ac901335991653829477c
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-20 13:37:03 -0400

    Deleting a task

--- src/App.jsx
+++ src/App.jsx
@@ -4,60 +4,78 @@ import NewProject from "./component/NewProject";
 import NoProjectSelected from "./component/NoProjectSelected";
 import SelectedProject from "./component/SelectedProject";
 function App() {
-  const [projects,setProjects] = useState({
+  const [projects, setProjects] = useState({
     selectedProjectID: undefined,
-    projects: []
+    projects: [],
   });
-  function handleStartedProject(){
-    setProjects(prevState => {
+  function handleStartedProject() {
+    setProjects((prevState) => {
       return {
         ...prevState,
         selectedProjectID: null,
-
-      }
-    })
+      };
+    });
   }
- function handleProjectAdd(inputData){
-  setProjects(prevState=> {
-    const projectID = Math.random();
-    let newProject = {
-      ...inputData,
-      id: projectID
-    };
-    return{
-      ...prevState,
-      selectedProjectID: undefined,
-      projects: [...prevState.projects,newProject]
-    }
-  })
- }
- function cancelProject(){
-   setProjects(prevState=> {
-     return{
-       ...prevState,
-       selectedProjectID: undefined,
-     }
-   })
- }
- function handleSelectProject(id){
-  setProjects((prevState)=>{
-    return {
-      ...prevState,
-      selectedProjectID: id
-    }
-  })
- }
- const selectedProject = projects.projects.find(project => project.id === projects.selectedProjectID)
+  function handleProjectAdd(inputData) {
+    setProjects((prevState) => {
+      const projectID = Math.random();
+      let newProject = {
+        ...inputData,
+        id: projectID,
+      };
+      return {
+        ...prevState,
+        selectedProjectID: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+  function cancelProject() {
+    setProjects((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: undefined,
+      };
+    });
+  }
+  function handleSelectProject(id) {
+    setProjects((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: id,
+      };
+    });
+  }
+  function handleDeleteProject() {
+    setProjects((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectID
+        ),
+      };
+    });
+  }
+  const selectedProject = projects.projects.find(
+    (project) => project.id === projects.selectedProjectID
+  );
   // console.log(projects)
-  let content = <SelectedProject project={selectedProject}/>;
-  if(projects.selectedProjectID === null){
-    content = <NewProject onAdd={handleProjectAdd} cancelProject ={cancelProject}/>
-  } else if(projects.selectedProjectID === undefined) {
-    content = <NoProjectSelected onStartedProject= {handleStartedProject} />
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  if (projects.selectedProjectID === null) {
+    content = (
+      <NewProject onAdd={handleProjectAdd} cancelProject={cancelProject} />
+    );
+  } else if (projects.selectedProjectID === undefined) {
+    content = <NoProjectSelected onStartedProject={handleStartedProject} />;
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onSelecetProject = {handleSelectProject} onStartedProject ={handleStartedProject} projects = {projects.projects}/>
+      <ProjectSidebar
+        onSelecetProject={handleSelectProject}
+        onStartedProject={handleStartedProject}
+        projects={projects.projects}
+      />
       {content}
     </main>
   );
--- src/component/SelectedProject.jsx
+++ src/component/SelectedProject.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SelectedProject = ({project}) => {
+const SelectedProject = ({project,onDelete}) => {
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-us',{
         year: 'numeric',
         month: 'short',
@@ -11,7 +11,7 @@ const SelectedProject = ({project}) => {
         <header className='pb-4 mb-4 border-b-2 border-gray-100'>
             <div className='flex items-center justify-between'>
                 <h1 className='text-3xl font-bold text-white'>{project.title}</h1>
-                <button className='text-white'>Delete</button>
+                <button className='text-white' onClick={onDelete}>Delete</button>
             </div>
             <p className='mb-4 text-white'>{formattedDate}</p>
             <p className='whitespace-pre-wrap text-white'>{project.description}</p>
