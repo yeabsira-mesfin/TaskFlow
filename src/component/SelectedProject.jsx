58b86dea706966926c0d41465329b8850ecbcdae
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-06-20 15:08:13 -0400

    Finalized delete button hover

--- src/component/SelectedProject.jsx
+++ src/component/SelectedProject.jsx
@@ -11,7 +11,7 @@ const SelectedProject = ({project,onDelete, onAddTask, onDeleteTask,tasks}) => {
         <header className='pb-4 mb-4 border-b-2 border-gray-100'>
             <div className='flex items-center justify-between'>
                 <h1 className='text-3xl font-bold text-white'>{project.title}</h1>
-                <button className='text-white' onClick={onDelete}>Delete</button>
+                <button className='text-white hover:text-red-700' onClick={onDelete}>Delete</button>
             </div>
             <p className='mb-4 text-white'>{formattedDate}</p>
             <p className='whitespace-pre-wrap text-white'>{project.description}</p>
